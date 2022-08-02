bb4056110a70756ffc636a421319eb7c847ad6f8
sunidhi-chauhan <sunidhichauhan780@gmail.com>
2022-08-02 18:57:15 +0530

    firstday

---
+++ src/com/sunidhi/Demo.java
@@ -0,0 +1,17 @@
+package com.sunidhi;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Name:Sunidhi Chauhan");
+		System.out.println("Father's Name : Omprakash Chauhan");
+		System.out.println("Mother's Name : Savita Chauhan");
+		System.out.println("Gender : Female");
+		System.out.println("Address : Dehradun");
+		System.out.println("mobile No : 9760941015");
+	
+	}
+
+}
