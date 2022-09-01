cb408b9e3d607024ee3dcd61c6357b7c092b1997
sunidhi-chauhan <102180876+sunidhi-chauhan@users.noreply.github.com>
2022-09-01 22:48:38 +0530

    Day5
    Day5

---
+++ src/com/Day5/Circle.java
@@ -0,0 +1,11 @@
+package com.Day5;
+
+public class Circle {
+	int radius;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/Day5/Rectangle.java
@@ -0,0 +1,14 @@
+package com.Day5;
+
+public class Rectangle {
+	 int length;
+     int breadth;
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
