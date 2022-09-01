499a3dd52990cc6ba42eeb06fe69b9506af983d6
sunidhi-chauhan <102180876+sunidhi-chauhan@users.noreply.github.com>
2022-09-01 22:38:27 +0530

    Day5

---
+++ src/com/Eva/src/com/Day5/Main2.java
@@ -0,0 +1,16 @@
+package com.Day5;
+
+public class Main2 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Shapes obj=new Shapes();
+		
+	obj.area(3);
+	obj.area(5,6);
+	obj.area(7);
+		
+	}
+
+}
